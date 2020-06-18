67fd12fd99181c920302c39d89596d56321f60df
= <sonoda_umi@163.com>
2020-06-18 11:06:21 +0800

    leetcode第35题搜索插入位置

---
+++ leetcode/algorithm/35.py
@@ -0,0 +1,34 @@
+# 35.搜索插入位置
+
+class Solution:
+    """
+    考察的是查找，可说的是各类查找方法（二分、快排等）
+    """
+    def searchInsertByBinary(self, nums, target) -> int:
+        """
+        二分法查找
+        :param nums: List[int]
+        :param target: int
+        :return:
+        """
+        if nums:
+            if target <= nums[0]:
+                return 0
+            if target > nums[-1]:
+                return len(nums)
+            ln = 0
+            rn = len(nums) - 1
+            mid = rn // 2
+            while ln < mid:
+                if target == nums[mid]:
+                    return mid
+                elif target > nums[mid]:
+                    ln = mid
+                    mid = (rn + ln) // 2
+                else:
+                    rn = mid
+                    mid = (rn + ln) // 2
+            return mid + 1
+        else:
+            return False
+
